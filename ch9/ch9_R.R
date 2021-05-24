c97518887f250a398eeacf1c52ce83a43b2b8fc0
cdwren <colindwren@gmail.com>
2021-05-24 09:57:02 -0600

    Update ch9_R.R
    Added header and adjusted for weighted-trade-choice

--- ch9/ch9_R.R
+++ ch9/ch9_R.R
@@ -1,3 +1,7 @@
+#Data analysis script for NetLogo output
+#A set of visualizations for a standard table output of NetLogo: scatterplot, barplot, and lineplot.
+#This is an example script used in chapter 9 of Romanowska, I., Wren, C., Crabtree, S. 2021. Agent-Based Modeling for Archaeology: Simulating the Complexity of Societies. Santa Fe, NM: SFI Press.
+#Code blocks: 9.6, 9.8, 9.10, 9.12
 
 # If you do not have the packages listed, uncomment the next line and run it once
 # install.packages("ggplot2","dplyr","readr")
@@ -6,21 +10,21 @@ library(readr)
 library(ggplot2)
 library(dplyr)
 
-data <- read_csv("Gaul_Model experiment-table.csv", skip = 6)
+simdata <- read_csv("Gaul_Model experiment-table.csv", skip = 6)
 
 # X-Y plot
-ggplot(data,aes(reproduction,`count turtles`)) +
+ggplot(simdata,aes(`weighted-trade-choice`,`count turtles`)) +
   geom_point()
 
 # Bar plot
-data %>%
-  group_by(reproduction, `metals?`) %>%
+simdata %>%
+  group_by(`weighted-trade-choice`, `metals?`) %>%
   mutate(mean_turtles = mean(`count turtles`)) %>%
-  ggplot(aes(reproduction,`count turtles`,fill = `metals?`)) +
+  ggplot(aes(`weighted-trade-choice`,`count turtles`,fill = `metals?`)) +
   geom_col(position = "dodge")
 
 # Wine curve plot
-curve_data <- read_csv("metals_ true_repro2_13.csv", skip = 17) %>%
+curve_data <- read_csv("metals_ true_repro3_13.csv", skip = 17) %>%
   rename(Etruscan = y) %>%
   rename(Greek = y_1)
 
@@ -28,4 +32,4 @@ ggplot(curve_data) +
   geom_line(aes(x,Etruscan), color = "blue") +
   geom_line(aes(x_1,Greek), color = "orange") 
 
-# To export any of these plots, use the ggsave() command 
+# To export any of these plots, use the ggsave() command after calling the plot
